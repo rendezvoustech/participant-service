6fede78f87c0da0a1af1dda12ab3c8d740b86d4a
Mikkel Arentoft <mikkel@mikadocs.com>
2022-12-08 20:11:43 +0100

    Added logging and telemetry

--- src/main/java/tech/rendezvous/participantservice/api/ParticipantRestController.java
+++ src/main/java/tech/rendezvous/participantservice/api/ParticipantRestController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.*;
 import tech.rendezvous.participantservice.domain.Participant;
 import tech.rendezvous.participantservice.domain.ParticipantModel;
 import tech.rendezvous.participantservice.domain.ParticipantService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.Valid;
 
 @RestController
 @RequestMapping("participants")
 public class ParticipantRestController {
+    private static final Logger log = LoggerFactory.getLogger(ParticipantRestController.class);
     private final ParticipantService participantService;
 
     public ParticipantRestController(ParticipantService participantService) {
@@ -19,28 +22,33 @@ public class ParticipantRestController {
 
     @GetMapping
     public Iterable<Participant> getParticipants() {
+        log.info("Fetching the list of participants");
         return participantService.viewList();
     }
 
     @GetMapping("{id}")
     public Participant getParticipant(@PathVariable Long id) {
+        log.info("Fetching the participant with id {} from the database", id);
         return participantService.viewDetails(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Participant post(@RequestBody @Valid ParticipantModel model) {
+        log.info("Adding a new participant to the catalog with name {}", model.name());
         return participantService.add(model);
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
+        log.info("Deleting participant with id {}", id);
         participantService.remove(id);
     }
 
     @PutMapping("{id}")
     public Participant put(@PathVariable Long id, @RequestBody @Valid ParticipantModel model) {
+        log.info("Updating participant with id {}", id);
         return participantService.editDetails(id, model);
     }
 }
